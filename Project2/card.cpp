64966ab74b13bcc146416fefc76f4351079d87e3
Austin, Izzy <iaustin@butler.edu>
2022-09-13 14:39:32 -0400

    starting Project 2

---
+++ Project2/card.cpp
@@ -0,0 +1,6 @@
+#include "card.h"
+
+std::string Card::SUIT[] = {"No Suit", "Spades", "Hearts", "Diamonds", "Clubs"};
+
+std::string Card::FACE[] = {"Joker", "Ace", "Two", "Three", "Four", "Five", "Six",  
+                       "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
---
+++ Project2/driverCard.cpp
@@ -0,0 +1,10 @@
+// Honor Pledge:
+//
+// I pledge that I have neither given not
+// received any help on this assignment
+//
+// iaustin
+
+#include <iostream>
+#include <fstream> // File Stream Library
+#include "card.h"
